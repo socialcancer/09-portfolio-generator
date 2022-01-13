0b2aae41a13357782a3fac26249090fa1f73629a
dani <photomassacre@gmail.com>
2022-01-13 15:19:57 -0800

    Learning about node.js, const, let, and .forEach for arrays

---
+++ app.js
@@ -0,0 +1,22 @@
+// const animalArray = ['dog', 'cat', 'pig'];
+
+// animalArray.push('cow');
+
+// const personObj = {
+//     name: 'Daniel',
+//     age: 40
+// };
+
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
+
+// console.log(animalArray);
+// console.log(personObj);
+
+// const printProfileData = (profileDataArr) => {
+//     console.log(profileDataArr);
+// };
+
+//notice the lack of parentheses aroun the `profileDataArr` parameter?
+profileDataArr.forEach(profileItem => console.log(profileItem));
+

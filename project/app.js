836ca6df453cbf1f3a6609f631e8d7a777a01b20
dani <photomassacre@gmail.com>
2022-01-16 12:23:59 -0800

    Getting back to knocking out portfolio work

--- project/app.js
+++ project/app.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
 

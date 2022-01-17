1bd04175d9f5284ccd23e5d69ac6d4aa09057d38
dani <photomassacre@gmail.com>
2022-01-16 20:52:22 -0800

    Using inquirer for prompts

--- project/app.js
+++ project/app.js
@@ -1,19 +1,15 @@
-
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+const inquirer = require('inquirer');
 
 
 
-// console.log(name, github);
-// console.log(generatePage(name, github));
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ]);
+}
 
+promptUser().then(answers => console.log(answers));

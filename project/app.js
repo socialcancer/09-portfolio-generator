52b0699a512e65afb1d9fd520a85aba75c910076
dani <photomassacre@gmail.com>
2022-01-19 14:51:39 -0800

    Pushing additional changes before going into the next lesson

--- project/app.js
+++ project/app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -126,14 +127,16 @@ Add a New Project
         });
 };
 
+
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
         console.log(portfolioData);
-        // will be uncommented in lesson 4
-        // const pageHTML = generatePage(portfolioData);
-        // fs.writeFile('./index.html', pageHTML, err => {
-        //   if (err) throw new Error(err);
-        //   console.log('Page created! Check out index.html in this directory to see it!');
-        // });
+        //will be uncommented in lesson 4
+        const pageHTML = generatePage(portfolioData);
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
